a3f1f95e0a0b5e30eb458e7309f50040b60e3740
Muaz Muhammad <muazmuhammad21@gmail.com>
2024-08-02 21:41:59 +0600

    second commit

--- script.js
+++ script.js
@@ -1,34 +1,28 @@
-document.getElementById('calculateBtn').addEventListener('click',calculateLoan);
+document.getElementById('calculateBtn').addEventListener('click', calculateLoan);
 
+function calculateLoan() {
+    const loanAmount = parseFloat(document.getElementById("loanAmountInput").value);
+    const interestRate = parseFloat(document.getElementById("interestRateInput").value);
+    const loanTerm = parseFloat(document.getElementById("loanTermInput").value);
 
-function calculateLoan(){
-
-
-const loanAmount = parseFloat(document.getElementById("loanAmountInput").value);
-const interestRate = parseFloat(document.getElementById("interestRateInput").value);
-const loanTerm = parseFloat(document.getElementById("loanTermInput").value);
-
-
-    if(isNaN(loanAmount) || isNaN(interestRate) || isNaN(loanTerm)){
-        alert("Please Enter valid Numbers for all the Fields");
+    if (isNaN(loanAmount) || isNaN(interestRate) || isNaN(loanTerm)) {
+        alert("Please enter valid numbers for all fields");
+        return;
     }
 
-    const monthlyInterest = interestRate /100 / 12;
+    const monthlyInterest = interestRate / 100 / 12;
     const totalPayments = loanTerm;
-    const monthlyPayment = (loanAmount * monthlyInterest)/(1-Math.pow(1 + monthlyInterest, -totalPayments));
-    const totalInterst = (monthlyPayment * totalPayments) - loanAmount;
+    const monthlyPayment = (loanAmount * monthlyInterest) / (1 - Math.pow(1 + monthlyInterest, -totalPayments));
+    const totalInterest = (monthlyPayment * totalPayments) - loanAmount;
 
-
-    displayResult(monthlyPayment,totalInterst);
-
+    displayResult(monthlyPayment, totalInterest);
 }
 
-function displayResult(monthlyPayment, totalInterst){
-
+function displayResult(monthlyPayment, totalInterest) {
     const resultDiv = document.getElementById('result');
 
     resultDiv.innerHTML = `
-        <p><strong>Monthly Payment: ${monthlyPayment.toFixed(2)}</p></strong>
-        <p><strong>Total Interest: ${totalInterst.toFixed(2)}</p></strong>
+        <p><strong>Monthly Payment:</strong> ${monthlyPayment.toFixed(2)}</p>
+        <p><strong>Total Interest:</strong> ${totalInterest.toFixed(2)}</p>
     `;
 }
